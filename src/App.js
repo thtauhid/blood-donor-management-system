6c2f8bf44f9bcd267e0365cdce2f2071dec3c400
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-10-24 04:36:33 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from "./Home"
 import DonorList from "./DonorList"
+import CreateDonor from "./CreateDonor"
 // import Try from "./Try.js"
 
 const App = () => {
   return (
     <React.Fragment>
       <Router>
-        <Route to="/" component={DonorList} />
+        <Route path="/" exact component={Home} />
+        <Route path="/create" component={CreateDonor} />
+        <Route path="/donors" component={DonorList} />
       </Router>
     </React.Fragment>
   )
