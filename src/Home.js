9d62cca0ce8117f41265a178addf8404c599f7a4
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-10-24 06:28:00 +0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <Link to="/donors">
+        <button>Donors</button>
+      </Link>
+      <br />
+      <Link to="/donors/create">
+        <button>Add Donor</button>
+      </Link>
+      <br />
+      <Link to="/donations">
+        <button>Donation</button>
+      </Link>
+      <br />
+      <Link to="/donations/create">
+        <button>Add Donation</button>
+      </Link>
+      <br />
+    </div>
+  )
 }
 
 export default Home
