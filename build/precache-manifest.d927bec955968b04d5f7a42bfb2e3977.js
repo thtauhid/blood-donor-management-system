b116b5765e01f6cbc08466b5f579460d413d59bf
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-10-24 18:19:54 +0600

    Build

---
+++ build/precache-manifest.d927bec955968b04d5f7a42bfb2e3977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976e7777e210d05cb394c4c9da2724dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345a41412a04afcc21cc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd0856364722003c30db",
+    "url": "/static/js/2.321d1223.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/static/js/2.321d1223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345a41412a04afcc21cc",
+    "url": "/static/js/main.e4a4dc04.chunk.js"
+  },
+  {
+    "revision": "4e54f4f83c3b4c053dba",
+    "url": "/static/js/runtime-main.e0154d62.js"
+  }
+]);
