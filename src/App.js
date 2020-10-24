f06f685cf73a13a776b21faa29daddcee89c6b61
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-10-24 18:19:09 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,9 @@ import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import Typography from "@material-ui/core/Typography"
-import MenuIcon from "@material-ui/core/Menu"
 
 import HomeIcon from "@material-ui/icons/Home"
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
-import LockOpenOutlinedIcon from "@material-ui/icons/LockOpenOutlined"
 
 const { netlifyIdentity } = window
 
