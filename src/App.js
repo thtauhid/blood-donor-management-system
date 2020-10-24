1215a202ff962d7f2a7008914534d074a077a64d
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-10-24 17:27:08 +0600

    Netlify Identity implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./Home"
 import DonorList from "./DonorList"
@@ -7,9 +7,52 @@ import Donations from "./Donations"
 import CreateDonation from "./CreateDonation"
 // import Try from "./Try.js"
 
+const { netlifyIdentity } = window
+
+const login = () => {
+  netlifyIdentity.open()
+}
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [userName, setUserName] = useState("")
+
+  // not needed at this moment as app is invite only.
+  // const [isAdmin, setIsAdmin] = useState(false)
+
+  // Checks if user is logged in
+  netlifyIdentity.on("init", (user) => {
+    if (user) {
+      setLoggedIn(true)
+
+      // Check if user is admin
+      // not needed at this moment as app is invite only.
+      // const userRole = user.app_metadata.roles[0]
+      // if (userRole === "Admin") {
+      //   setIsAdmin(true)
+      // }
+
+      // Set username
+      setUserName(user.user_metadata.full_name)
+    } else {
+      setLoggedIn(false)
+    }
+  })
+  // Triggers when user logs in
+  netlifyIdentity.on("login", (user) => {
+    setLoggedIn(true)
+    // Set username
+    setUserName(user.user_metadata.full_name)
+  })
+
+  // Triggers when user logs out
+  netlifyIdentity.on("logout", () => {
+    setLoggedIn(false)
+  })
+
   return (
     <React.Fragment>
+      {loggedIn ? `Logged In as ${userName}` : `Logged Out`}
+      <button onClick={login}>Login</button>
       <Router>
         <Route path="/" exact component={Home} />
         <Route path="/donors" exact component={DonorList} />
