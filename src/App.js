d399d82a3c80cfbf4246d8d03f9d1583a1dc083a
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-11-06 21:36:21 +0600

    Donor Profile Fix

--- src/App.js
+++ src/App.js
@@ -1,59 +1,61 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import { Layout } from 'antd'
-import Sidebar from './Sidebar'
-import 'antd/dist/antd.css'
+import React from "react"
+import { Route } from "react-router-dom"
+import { Layout } from "antd"
+import Sidebar from "./Sidebar"
+import "antd/dist/antd.css"
 
-import DonorList from './DonorList'
-import CreateDonor from './CreateDonor'
-import DonationList from './DonationList'
-import CreateDonation from './CreateDonation'
-import DonorProfile from './DonorProfile'
+import DonorList from "./DonorList"
+import CreateDonor from "./CreateDonor"
+import DonationList from "./DonationList"
+import CreateDonation from "./CreateDonation"
+import DonorProfile from "./DonorProfile"
 
 const { Content, Footer } = Layout
 
 const Routes = () => {
-	return (
-		<>
-			<Route path='/' exact component={DonorList} />
-			<Route path='/donor/create' component={CreateDonor} />
-			<Route path='/donor/:id' component={DonorProfile} />
-			<Route path='/donations' exact component={DonationList} />
-			<Route path='/donation/create' component={CreateDonation} />
-		</>
-	)
+  return (
+    <>
+      <Route path="/" exact component={DonorList} />
+      <Route path="/donor/create" exact component={CreateDonor} />
+      <Route path="/donor/profile/:id" component={DonorProfile} />
+      <Route path="/donations" exact component={DonationList} />
+      <Route path="/donation/create" component={CreateDonation} />
+    </>
+  )
 }
 
 const App = () => {
-	return (
-		<>
-			<Layout style={{ minHeight: '100vh' }}>
-				<Sidebar />
-				<Layout className='site-layout'>
-					<Content style={{ margin: '0 16px' }}>
-						<div
-							className='site-layout-background'
-							style={{ padding: 24, minHeight: 360 }}>
-							<Routes />
-						</div>
-					</Content>
-					<Footer style={{ textAlign: 'center' }}>
-						Coded with{' '}
-						<span role='img' aria-label='love'>
-							🖤
-						</span>
-						by
-						<a
-							href='https://github.com/thtauhid'
-							target='_blank'
-							rel='noopener noreferrer'>
-							Tauhid
-						</a>
-					</Footer>
-				</Layout>
-			</Layout>
-		</>
-	)
+  return (
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sidebar />
+        <Layout className="site-layout">
+          <Content style={{ margin: "0 16px" }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Routes />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Coded with{" "}
+            <span role="img" aria-label="love">
+              🖤
+            </span>
+            by{"  "}
+            <a
+              href="https://github.com/thtauhid"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tauhid
+            </a>
+          </Footer>
+        </Layout>
+      </Layout>
+    </>
+  )
 }
 
 export default App
--- src/DonorList.js
+++ src/DonorList.js
@@ -1,59 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import { Table, Breadcrumb } from 'antd'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react"
+import { Table, Breadcrumb } from "antd"
+import { Link } from "react-router-dom"
 
-import api from './api'
+import api from "./api"
 
 const DonorList = () => {
-	const [fullData, setFullData] = useState([''])
+  const [fullData, setFullData] = useState([""])
 
-	useEffect(() => {
-		api
-			.getDonors()
+  useEffect(() => {
+    api
+      .getDonors()
 
-			.then((donors) => {
-				const fullData = donors.map((donor) => {
-					const { id: key } = donor.ref['@ref']
-					const { name, blood_group, phone_number } = donor.data
-					return {
-						key,
-						name,
-						blood_group,
-						phone_number,
-					}
-				})
+      .then((donors) => {
+        const fullData = donors.map((donor) => {
+          const { id: key } = donor.ref["@ref"]
+          const { name, blood_group, phone_number } = donor.data
+          return {
+            key,
+            name,
+            blood_group,
+            phone_number,
+          }
+        })
 
-				setFullData(fullData)
-				console.log(fullData)
-			})
-	}, [])
-	const columns = [
-		{
-			title: 'Name',
-			dataIndex: 'name',
-			key: 'name',
-			render: (text, record) => <Link to={`/donor/${record.key}`}>{text}</Link>,
-		},
-		{
-			title: 'Blood Group',
-			dataIndex: 'blood_group',
-			key: 'blood_group',
-		},
-		{
-			title: 'Contact Number',
-			dataIndex: 'phone_number',
-			key: 'phone_number',
-		},
-	]
-	return (
-		<>
-			<Breadcrumb style={{ margin: '16px 0' }}>
-				<Breadcrumb.Item>Donor</Breadcrumb.Item>
-				<Breadcrumb.Item>List</Breadcrumb.Item>
-			</Breadcrumb>
-			<Table columns={columns} dataSource={fullData} />
-		</>
-	)
+        setFullData(fullData)
+        console.log(fullData)
+      })
+  }, [])
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text, record) => (
+        <Link to={`/donor/profile/${record.key}`}>{text}</Link>
+      ),
+    },
+    {
+      title: "Blood Group",
+      dataIndex: "blood_group",
+      key: "blood_group",
+    },
+    {
+      title: "Contact Number",
+      dataIndex: "phone_number",
+      key: "phone_number",
+    },
+  ]
+  return (
+    <>
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb.Item>Donor</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+      </Breadcrumb>
+      <Table columns={columns} dataSource={fullData} />
+    </>
+  )
 }
 
 export default DonorList
